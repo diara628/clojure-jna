1894aceea9f4cda6f9c4164ce1228717b0aa561c
Chris Nuernberger <cnuernber@gmail.com>
2009-11-30 03:28:11 +0800

    Added some more functionality around libraries that are dynamically created and thus not in LD_LIBRARY_PATH

--- src/net/n01se/clojure_jna.clj
+++ src/net/n01se/clojure_jna.clj
@@ -5,12 +5,14 @@
 ; By using this software in any fashion, you are agreeing to be bound by
 ; the terms of this license.
 ; You must not remove this notice, or any other, from this software.
+; Some extensions programmed by:
+; Chris Nuernberger
 
 (ns
-  #^{:author "Chris Houser"
+  #^{:author "Chris Houser et al."
      :doc "Dynamically load and use native C libs from Clojure using JNA"}
   net.n01se.clojure-jna
-  (:import (com.sun.jna Native)))
+  (:import (com.sun.jna Native Function)))
 
 (defn- get-function [s]
   `(com.sun.jna.Function/getFunction ~(namespace s) ~(name s)))
@@ -66,3 +68,128 @@
   (if (== -1 value)
     (throw (Exception. (str msg ", errno: " (Native/getLastError))))
     value))
+
+(defn jna-malloc
+  "Malloc a little endian byte buffer.  Returns
+both the buffer and pointer with one call.
+Found from a web-page:
+http://nakkaya.com/2009/11/16/java-native-access-from-clojure/
+-CN
+"
+  [byte-size] 
+  (let [buffer (make-cbuf byte-size)
+	pointer (pointer buffer)]
+    {:pointer pointer :buffer buffer}))
+
+(defn get-jna-fn
+  "Get a function object from a given library.
+libname - either short name, 'cudart' or long full pathname
+fnname - name of the function
+-CN"
+  [lib-name fn-name]
+  (Function/getFunction lib-name fn-name))
+
+(defn jna-fn-to-fn
+  "Given a jna function and a return type,
+return a clojure function that will call the jna
+function.
+-CN"
+  ([jna-fn rettype]
+     (fn [& args]
+       (.invoke jna-fn rettype (to-array args))))
+  ([jna-fn]
+     (fn [& args]
+       (.invoke jna-fn (to-array args)))))
+
+(defn create-jna-fn-def 
+  "Given a function name, return type, and argument
+datatypes, return a jna function definition.  Argument datatypes
+are java primitive types, i.e. Float, Integer, Short, etc.
+Pointers to those types are denoted by an array with :pointer
+keywords for each level of indirection.  Thus you would have 
+
+ (create-jna-fn 'addFloats' Float [:pointer Float] Integer)
+
+which would create a jna function definition that summed an
+array of floats of an integer length and returned the resulting
+float.
+
+Rettype may be nil in which case a void return value is assumed
+-CN"
+  [fn-name rettype & arg-types]
+  (let [retval {:name fn-name
+		:arguments arg-types}]
+    (if rettype
+      (assoc retval :return-type rettype)
+      retval)))
+
+(defn create-namespaced-jna-fn-def
+  "See create-jna-fn-def.
+Additional argument is a sequence of string namespace
+names.  These are not clojure namespaces but c++
+namespace names you have in the code.
+ (def fdef (create-namespaced-jna-fn-def 
+            [\"test1\" \"test2\"] 
+           \"add_floatNumber\" 
+            Float 
+            Float Float))
+-CN
+"
+  [namespaces fn-name rettype & arg-types]
+  (let [retval (apply create-jna-fn-def fn-name rettype arg-types)]
+    (assoc retval :namespaces namespaces)))
+
+(defn- maybe-to-seq [arg]
+  (try
+   (seq arg)
+   (catch Exception e
+     nil)))
+
+(def typename-map
+     { Float "f"
+      Integer "i"
+      :pointer "P" })
+
+(defn get-cpp-mangled-name 
+  "Return the c++ mangled name for a given
+function definition.  Please help fill out the
+typename map so we support more types.
+
+Result with namespaces:
+input:
+ {:namespaces [\"test1\" \"test2\"], :return-type java.lang.Float, :name \"add_floatNumber\", :arguments ([:pointer java.lang.Float] java.lang.Integer)}
+
+output:
+_ZN5test15test215addFloatNumbersEPfi
+
+
+Result without namespaces:
+input:
+ {:return-type java.lang.Float, :name \"add_floatNumber\", :arguments (java.lang.Float java.lang.Float)}
+
+output:
+_Z15add_floatNumberff
+-CN
+"
+  [jna-fn-def]
+  (let [namelen (count (:name jna-fn-def))
+	builder (StringBuilder.)
+	has_namespaces (not (nil? (:namespaces jna-fn-def)))]
+    (.append builder "_Z")
+    (when has_namespaces
+      (.append builder "N"))
+    (doseq [namespace (:namespaces jna-fn-def)]
+      (.append builder (.length namespace))
+      (.append builder namespace))
+    (.append builder namelen)
+    (.append builder (:name jna-fn-def))
+    (when has_namespaces
+      (.append builder "E"))
+    (if (seq? (:arguments jna-fn-def))
+      (doseq [arg (:arguments jna-fn-def)]
+	(if (maybe-to-seq arg)
+	  (doseq [part arg]
+	    (.append builder (typename-map part)))
+	  (.append builder (typename-map arg))))
+      (.append builder "v"))
+    (.toString builder)))
